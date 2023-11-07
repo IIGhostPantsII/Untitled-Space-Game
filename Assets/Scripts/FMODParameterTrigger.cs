4ccc6def2c835084ebbb1f2c795cb16f85fba200
IIGhostPantsII <david.cacorovski000@gmail.com>
2023-11-06 21:36:05 -0500

    Stuff

---
+++ Assets/Scripts/FMODParameterTrigger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODParameterTrigger : MonoBehaviour
+{
+    //THIS SHOULD JUST BE A TEMPORARY SCRIPT IM JUST VERY ANGRY AND IF THIS IS STILL HERE LATER PLS DELETE AND FIND A BETTER WAY THANK YOU
+
+    public PlayerController _playerMovement;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _playerMovement.moveEvent.setParameterByName("Pitch", 10);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _playerMovement.moveEvent.setParameterByName("Pitch", 0);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,37 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class PlayerController : MonoBehaviour
 {
-    //Settings you can change in the inspector
+    public FMODUnity.EventReference moveEventPath;
+    public FMOD.Studio.EventInstance moveEvent;
+    private FMOD.Studio.PLAYBACK_STATE playbackState;
+    private float timeSinceLastPlay = 0.0f;
+    public float eventInterval = 0.5f; // Adjust this to change the interval
+    private bool isEventPlaying = false;
+    private bool isFirstInput = true; // Track the first input
+
+    // Settings you can change in the inspector
     [SerializeField] private float _speed = 800f;
     [SerializeField] private float _sprintMultiplier = 1.5f;
     [SerializeField] private float _jumpForce = 15f;
-    public float _mouseSensitivity = 5f;
     [SerializeField] private float _groundCheckRadius = 1.0f;
     [SerializeField] private LayerMask _groundLayer;
-    [SerializeField] private Transform _cameraTransform;
-    //INPUT BOOLS
+
+    // INPUT BOOLS
     private bool _isSprinting;
     private bool _isJumping;
     private bool _isGrounded;
     private bool _isDashing;
 
-    //Using Unitys new input system
+    // Using Unity's new input system
     PlayerInput _input;
 
     private Rigidbody _playerRigidbody;
-
-    //Rotation - used for mouse input
-    float _xRotation = 0f;
 
     bool _delay;
     public bool _shootDelay;
     public bool _reload;
     public bool _noAni;
-
     public int _ammo;
+
+    private bool isMoving = false;
 
     private void Awake()
     {
@@ -43,33 +49,63 @@ public class PlayerController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         _playerRigidbody = GetComponent<Rigidbody>();
+
+        // Initialize the FMOD event instance
+        moveEvent = RuntimeManager.CreateInstance(moveEventPath);
     }
 
     void Update()
     {
-        //INPUT VALUES
+        // INPUT VALUES
         _isJumping = _input.Player.Jump.ReadValue<float>() > 0.1f;
         _isSprinting = _input.Player.Sprint.ReadValue<float>() > 0.1f;
         _isDashing = _input.Player.Dash.ReadValue<float>() > 0.1f;
 
         _isGrounded = Physics.SphereCast(transform.position, _groundCheckRadius, -Vector3.up, out RaycastHit hitInfo, 0.1f, _groundLayer);
 
-        if(_isJumping && !_delay && _isGrounded)
+        if (_isJumping && !_delay && _isGrounded)
         {
             StartCoroutine(InputDelay(0.15f));
             _delay = true;
             StartCoroutine(Jump());
         }
 
-        float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity * Screen.dpi * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity * Screen.dpi * Time.deltaTime;
+        // Track the time since the event last played
+        if (isMoving)
+        {
+            timeSinceLastPlay += Time.deltaTime;
 
+            // If it's the first input, use half the event interval
+            if (isFirstInput && timeSinceLastPlay >= eventInterval / 2)
+            {
+                isFirstInput = false;
+            }
+        }
+        else
+        {
+            // Reset the time since the event last played and the first input flag when the player stops moving
+            timeSinceLastPlay = 0.0f;
+            isFirstInput = true;
+        }
 
-        _xRotation -= mouseY;
-        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+        // Play the FMOD event if the player is moving and enough time has passed
+        if (isMoving && timeSinceLastPlay >= (isFirstInput ? eventInterval / 2 : eventInterval))
+        {
+            moveEvent.start();
+            timeSinceLastPlay = 0.0f;
+        }
 
-        _cameraTransform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
-        transform.Rotate(Vector3.up * mouseX);
+        // Stop the FMOD event when the player has finished moving and the event is playing
+        if (!isMoving)
+        {
+            FMOD.Studio.PLAYBACK_STATE playbackState;
+            moveEvent.getPlaybackState(out playbackState);
+
+            if (playbackState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+            {
+                moveEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            }
+        }
     }
 
     void FixedUpdate()
@@ -81,21 +117,31 @@ public class PlayerController : MonoBehaviour
         movement = transform.TransformDirection(movement);
         movement.y = 0;
 
-        if(_isSprinting)
+        if (_isSprinting)
         {
             movement *= _sprintMultiplier;
         }
 
-        if(_isDashing && !_delay && movement.magnitude > 0)
+        if (_isDashing && !_delay && movement.magnitude > 0)
         {
             StartCoroutine(InputDelay(3.0f));
             _delay = true;
             StartCoroutine(Dash(movement));
         }
 
-        if(!_isGrounded)
+        if (!_isGrounded)
         {
             movement.y -= 9.8f * Time.deltaTime;
+        }
+
+        // Check if the player is moving
+        if(Mathf.Abs(horizontal) > 0.1f || Mathf.Abs(vertical) > 0.1f)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
         }
 
         _playerRigidbody.velocity = movement * _speed * Time.deltaTime;
@@ -125,7 +171,7 @@ public class PlayerController : MonoBehaviour
 
     IEnumerator Dash(Vector3 soTrue)
     {
-        for(int i = 20; i < 40; i++)
+        for (int i = 20; i < 40; i++)
         {
             yield return new WaitForSeconds(0.01f);
             _playerRigidbody.AddForce(soTrue.normalized * i, ForceMode.Impulse);
@@ -134,7 +180,7 @@ public class PlayerController : MonoBehaviour
 
     IEnumerator Jump()
     {
-        for(int i = 5; i < _jumpForce; i++)
+        for (int i = 5; i < _jumpForce; i++)
         {
             yield return new WaitForSeconds(0.005f);
             _playerRigidbody.AddForce(Vector3.up * i, ForceMode.Impulse);
