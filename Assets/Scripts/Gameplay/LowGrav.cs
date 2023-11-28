3b7f65a295bd13ee4d71265e516ea70b1185fde9
David <david.cacorovski000@gmail.com>
2023-11-28 13:41:56 -0400

    uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

---
+++ Assets/Scripts/Gameplay/LowGrav.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LowGrav : MonoBehaviour
+{
+    /*
+    [SerializeField] private PlayerController playerScript;
+    [SerializeField] private float gravity;
+    [SerializeField] private float jumpForce;
+
+    private float tempGrav;
+    private float tempJumpForce;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            tempGrav = playerScript.gravity;
+            tempJumpForce = playerScript._jumpForce;
+            playerScript.gravity = gravity;
+            playerScript._jumpForce = jumpForce;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerScript.gravity = tempGrav;
+            playerScript._jumpForce = tempJumpForce;
+        }
+    }
+    */
+}
