0a58bfd2fc56daa5b219bb06e1fbcc6ce7038205
Mabel <luccadacambra@gmail.com>
2024-02-11 20:14:41 -0500

    Voiceline system! Subtitle system!

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using FMODUnity;
 
@@ -111,6 +112,30 @@ public static class Globals
                 eventInstance.setParameterByName("Lowpass",22000);
             }
         }
+    }
+    
+    public static Dictionary<string, ArrayList> LoadTSV(TextAsset file) {
+        Dictionary<string, ArrayList> dictionary = new Dictionary<string, ArrayList>();
+        ArrayList list = new ArrayList();
+        
+        var content = file.text;
+        var lines = content.Split(System.Environment.NewLine);
+
+        for (int i=0; i < lines.Length; i++)
+        {
+            list = new ArrayList();
+            var line = lines[i];
+            if (string.IsNullOrEmpty(line)) continue;
+            string[] values = line.Split('	');
+            for (int j=1; j < values.Length; j++) {
+                list.Add(values[j]);
+            }
+
+            values[0] = new string(values[0].Where(c => !char.IsControl(c)).ToArray());
+            if (values[0] != "") dictionary.Add(values[0], list);
+        }
+
+        return dictionary;
     }
 }
 
---
+++ Assets/VoicelineController.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using FMOD.Studio;
+using FMODUnity;
+using NaughtyAttributes;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class VoicelineController : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _subtitles;
+    [SerializeField] private TextAsset _voicelineTSV;
+    
+    private bool _isPlaying;
+    private GameObject _player;
+    private Dictionary<string, ArrayList> _voicelines = new Dictionary<string, ArrayList>();
+    private List<string> _usedVoiceLines = new List<string>();
+    
+    private void Start()
+    {
+        _voicelines = Globals.LoadTSV(_voicelineTSV);
+        _player = GameObject.FindWithTag("MainCamera");
+        _subtitles.SetText("");
+    }
+    
+    [Button]
+    void PlaySound()
+    {
+        System.Random rand = new System.Random();
+        StartCoroutine(PlayVoiceline($"msg_descole_test{Random.Range(1, 8)}"));
+    }
+
+    private IEnumerator PlayVoiceline(string sound)
+    {
+        if (_isPlaying) yield break;
+        if (_usedVoiceLines.Contains(sound))
+        {
+            Debug.LogError($"Voice line \"{sound}\" already played");
+            yield break;
+        }
+
+        if (!_voicelines.ContainsKey(sound) && !_voicelines.ContainsKey(sound + "_1")) sound = "err_fake_voiceline";
+        
+        _isPlaying = true;
+        
+        FMOD.Studio.EventInstance instance = RuntimeManager.CreateInstance($"event:/Dialogue Sounds/PA Jingles/PA-Normal-Opening");
+        RuntimeManager.AttachInstanceToGameObject(instance, _player.transform, _player.GetComponent<Rigidbody>());
+        instance.start();
+        instance.release();
+
+        instance.getPlaybackState(out var state);
+        while (state != PLAYBACK_STATE.STOPPING && state != PLAYBACK_STATE.STOPPED)
+        {
+            yield return null;
+            instance.getPlaybackState(out state);
+        }
+
+        List<string> voiceLineSplit = sound.Split("_").ToList();
+        bool multipleVoiceClips = false;
+        int currentLine = 1;
+
+        if (_voicelines.ContainsKey(sound + "_1")) multipleVoiceClips = true;
+
+        try
+        {
+            currentLine = Int32.Parse(voiceLineSplit[^1]);
+            multipleVoiceClips = true;
+            voiceLineSplit.RemoveAt(voiceLineSplit.Count - 1);
+
+            sound = string.Join("_", voiceLineSplit);
+            Debug.Log(sound);
+        } catch {}
+
+        if (multipleVoiceClips)
+        {
+            while (true)
+            {
+                string tempSound = sound + "_" + currentLine;
+                
+                try
+                {
+                    _subtitles.SetText(_voicelines[tempSound][1].ToString());
+                    RuntimeManager.PlayOneShotAttached($"event:/Dialogue Sounds/Dialogue/{tempSound}",
+                        _player);
+                }
+                catch
+                {
+                    goto DoneVoiceLine;
+                }
+                
+                yield return new WaitForSeconds(0.075f);
+        
+                instance = RuntimeManager.CreateInstance($"event:/Dialogue Sounds/Dialogue/{tempSound}");
+                RuntimeManager.AttachInstanceToGameObject(instance, _player.transform, _player.GetComponent<Rigidbody>());
+                instance.start();
+                instance.release();
+
+                instance.getPlaybackState(out state);
+                while (state != PLAYBACK_STATE.STOPPING && state != PLAYBACK_STATE.STOPPED)
+                {
+                    yield return null;
+                    instance.getPlaybackState(out state);
+                }
+
+                currentLine += 1;
+            }
+        }
+        else
+        {
+            _subtitles.SetText(_voicelines[sound][1].ToString());
+        
+            RuntimeManager.PlayOneShotAttached($"event:/Dialogue Sounds/Dialogue/{sound}", _player);
+            yield return new WaitForSeconds(0.075f);
+        
+            instance = RuntimeManager.CreateInstance($"event:/Dialogue Sounds/Dialogue/{sound}");
+            RuntimeManager.AttachInstanceToGameObject(instance, _player.transform, _player.GetComponent<Rigidbody>());
+            instance.start();
+            instance.release();
+
+            instance.getPlaybackState(out state);
+            while (state != PLAYBACK_STATE.STOPPING && state != PLAYBACK_STATE.STOPPED)
+            {
+                yield return null;
+                instance.getPlaybackState(out state);
+            }
+        }
+
+        DoneVoiceLine:
+        yield return new WaitForSeconds(0.75f);
+        
+        _subtitles.SetText("");
+        instance = RuntimeManager.CreateInstance($"event:/Dialogue Sounds/PA Jingles/PA-Normal-Closing");
+        RuntimeManager.AttachInstanceToGameObject(instance, _player.transform, _player.GetComponent<Rigidbody>());
+        instance.start();
+        instance.release();
+
+        instance.getPlaybackState(out state);
+        while (state != PLAYBACK_STATE.STOPPING && state != PLAYBACK_STATE.STOPPED)
+        {
+            yield return null;
+            instance.getPlaybackState(out state);
+        }
+
+        _isPlaying = false;
+    }
+}
