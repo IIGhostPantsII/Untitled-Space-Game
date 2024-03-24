e4bcb9a03e9e8e39d8e58a2f29d5e088219efb24
IIGhostPantsII <david.cacorovski000@gmail.com>
2024-03-24 13:08:21 -0400

    functional eject

--- Assets/Scripts/Gameplay/EndgameLogic.cs
+++ Assets/Scripts/Gameplay/EndgameLogic.cs
@@ -5,15 +5,58 @@ using UnityEngine;
 public class EndgameLogic : MonoBehaviour
 {
     public static bool Started;
+    public static bool CanEndGame;
+
+    public static GameObject[] Monsters;
+    public static GameObject EndgameCam;
+    public static Animator DonutAni;
+
+    [SerializeField] public GameObject[] _monsters;
+    [SerializeField] private GameObject _cam;
+    [SerializeField] private Animator _donutAni;
 
     void Start()
     {
-        //StartEndgame();
+        Monsters = new GameObject[_monsters.Length];
+        EndgameCam = _cam;
+        DonutAni = _donutAni;
+        StartEndgame();
+        for(int i = 0; i < _monsters.Length; i++)
+        {
+            Monsters[i] = _monsters[i];
+        }
     }
 
-    public void StartEndgame()
+    public static void StartEndgame()
     {
         Started = true;
         Globals.ChangeMonsterState("Chase");
+    }
+
+    public static void EndGame()
+    {
+        for(int i = 0; i < Monsters.Length; i++)
+        {
+            Monsters[i].SetActive(false);
+        }
+        EndgameCam.SetActive(true);
+        DonutAni.Play("launch");
+        Globals.LockMovement();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Monster"))
+        {
+            CanEndGame = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Monster"))
+        {
+            CanEndGame = false;
+        }
     }
 }
