71727098ad6b78720164aa06eeebdac4b25abc24
Mabel <luccadacambra@gmail.com>
2024-03-24 19:35:57 -0400

    Fixed bug where if you leave the power switch after activating phase 1, everything breaks

--- Assets/Scripts/Misc/PowerButton.cs
+++ Assets/Scripts/Misc/PowerButton.cs
@@ -85,6 +85,7 @@ public class PowerButton : MonoBehaviour
             }
             
             OnActivate?.Invoke();
+            if (!_multiPress) gameObject.SetActive(false);
 
             if(_taskName != null && _buttonType == ButtonType.Place || _taskName != null && _buttonType == ButtonType.Pickup && _automatic)
             {
@@ -97,9 +98,6 @@ public class PowerButton : MonoBehaviour
             }
 
             if (!string.IsNullOrEmpty(_taskName)) FindObjectOfType<TaskManager>().IncrementTask(_taskName);
-
-            if (!_multiPress) enabled = false;
-
         }
     }
 
